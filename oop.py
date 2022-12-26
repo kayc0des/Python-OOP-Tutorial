d102cd3d753bd4e344b3581336370ca6f153880b
kayc0des <k.boafo@alustudent.com>
2022-12-26 16:50:27 +0200

    init method added to class

--- oop.py
+++ oop.py
@@ -1,5 +1,14 @@
-item1 = 'Phone'
-item1_price = 100
-item1_quantity = 5
-item1_price_total = item1_price * item1_quantity
+class Item:
+    def __init__(self, name: str, price: float, quantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+item1 = Item("Phone", 100, 5)
+
+item2 = Item("Laptop", 1000, 1)
+
 
