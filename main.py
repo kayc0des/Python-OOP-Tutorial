a73cbda97a8fb9588e0afe44b311a8f3df879498
kayc0des <k.boafo@alustudent.com>
2022-12-26 18:31:19 +0200

    save instances in an object

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class Item:
     # Class attribute: Pay rate after 20% discount
     pay_rate = 0.8 
 
+    all = []
+
     def __init__(self, name: str, price: float, quantity=0):
         """init runs as soon as an object of a class is instantiated."""
         
@@ -15,15 +17,26 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    def __repr__(self):
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
+
 item1 = Item("Phone", 100, 5)
 item2 = Item("Laptop", 1000, 3)
 item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
+
+for instance in Item.all:
+    print(instance.name)
 
