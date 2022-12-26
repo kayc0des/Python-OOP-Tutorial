5d758ee490d15ef578f372c86488d25c6cd0836e
kayc0des <k.boafo@alustudent.com>
2022-12-26 17:21:33 +0200

    total price

--- oop.py
+++ oop.py
@@ -1,5 +1,6 @@
 class Item:
     def __init__(self, name: str, price: float, quantity=0):
+        """init runs as soon as an object of a class is instantiated."""
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -8,7 +9,10 @@ class Item:
         return self.price * self.quantity
 
 item1 = Item("Phone", 100, 5)
+print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 1000, 1)
+print(item2.calculate_total_price())
+
 
 
